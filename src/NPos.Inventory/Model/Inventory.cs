b9c76627aca5a1a9fa7b59373ec734be166697b2
lkimleng <limk@hhbrown.com>
2026-09-11 13:25:07 -0400

    Add inventory class model

---
+++ src/NPos.Inventory/Model/Inventory.cs
@@ -0,0 +1,47 @@
+﻿namespace NPos.Inventory.Model
+{
+    public class Inventory
+    {
+        private readonly List<StockItem> _stockItems = [];
+
+        public double LowStockLevel { get; set; } = 5.0;
+
+        public StockItem[] GetStockItem(Guid productId)
+        {
+            return [.. _stockItems.Where(s => s.ProductId == productId)];
+        }
+
+        public StockItem[] GetStockItems()
+        {
+            return [.. _stockItems];
+        }
+
+        public double GetCurrentStockLevel(Guid productId)
+        {
+            if (_stockItems.Count == 0)
+            {
+                return 0.0;
+            }
+
+            return _stockItems
+                .Where(s => s.ProductId == productId)
+                .Sum(s => s.NumberInStock);
+        }
+
+        public bool IsLowStockLevel(Guid productId)
+        {
+            double currentStock = GetCurrentStockLevel(productId);
+            return currentStock <= LowStockLevel;
+        }
+
+        public decimal GetLatestStockPrice(Guid productId)
+        {
+            var latestStockItem = GetStockItem(productId)
+                .Where(s => s.NumberInStock > 0) // Get only stock in
+                .OrderByDescending(s => s.CreatedAt)
+                .FirstOrDefault();
+
+            return latestStockItem?.Cost ?? 0.0m;
+        }
+    }
+}
