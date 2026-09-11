431d9ebecc941d40deafc7131523d56adb7fec5e
lkimleng <limk@hhbrown.com>
2026-09-11 14:26:29 -0400

    Add defined inventory service interface for operations including create new product, stock entry, and get product cost.

---
+++ src/NPos.Application/Service/IInventoryService.cs
@@ -0,0 +1,12 @@
+﻿using NPos.Application.Model;
+using NPos.Inventory.Model;
+
+namespace NPos.Application.Service
+{
+    public interface IInventoryService
+    {
+        Task CreateNewProduct(string barcode, string name, ProductCategory category);
+        Task StockEntry(string barcode, decimal cost, double numberInStock);
+        Task<decimal> GetProductCostBy(ProductQuery query);
+    }
+}
