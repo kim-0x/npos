9ee8a03128fe30bc8cad540c41181a38970f068c
lkimleng <limk@hhbrown.com>
2026-09-11 12:55:11 -0400

    Add stock item

---
+++ src/NPos.Inventory/Model/StockItem.cs
@@ -0,0 +1,10 @@
+﻿namespace NPos.Inventory.Model
+{
+    public class StockItem
+    {
+        public double NumberInStock { get; set; }
+        public decimal Cost { get; set; }
+        public Guid ProductId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
