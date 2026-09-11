92ae90a473985c7ff58709e382c3e1fa05905c74
lkimleng <limk@hhbrown.com>
2026-09-11 11:15:05 -0400

    add product model

---
+++ src/NPos.Inventory/Model/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace NPos.Inventory.Model
+{
+    public class Product : IComparable<Product>
+    {
+        public Guid Id { get; set; }
+        public string Barcode { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string? Category { get; set; }
+        public int CompareTo(Product? other)
+        {
+            if (other is null) return 1;
+
+            bool hasMatchingId = Id.Equals(other.Id);
+            bool hasMatchingBarcode = Barcode.Equals(other.Barcode, StringComparison.OrdinalIgnoreCase);
+
+            if (hasMatchingBarcode || hasMatchingId) return 0;
+
+            int barcodeComparison = string.Compare(Barcode, other.Barcode, StringComparison.OrdinalIgnoreCase);
+            if (barcodeComparison != 0) return barcodeComparison;
+
+            return Id.CompareTo(other.Id);
+        }
+    }
+}
