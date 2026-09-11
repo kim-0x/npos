ef1a79edc6ae0ac440bb1af08d35106edd0e67f9
lkimleng <limk@hhbrown.com>
2026-09-11 16:47:29 -0400

    Add stock item repository implementation including get latest item cost and save stock item.

---
+++ src/NPos.Infrastructure/Repository/StockItemRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NPos.Infrastructure.Persistence;
+using NPos.Inventory.Abstraction.Repository;
+using NPos.Inventory.Model;
+
+namespace NPos.Infrastructure.Repository
+{
+    public class StockItemRepository(NPosContext nPosContext) : IStockItemRepository
+    {
+        public Task<double> GetCurrentStockLevelByProductId(Guid productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<decimal> GetLatestItemCostById(Guid productId)
+        {
+            return await nPosContext.StockItems
+                .AsNoTracking()
+                .Where(s => s.ProductId == productId && s.NumberInStock > 0)
+                .OrderByDescending(s => s.CreatedAt)
+                .Take(1)
+                .Select(s => s.Cost)
+                .FirstOrDefaultAsync();
+        }
+
+        public Task<StockItem[]> GetStockItemsByProductId(Guid productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SaveStockItem(StockItem stockItem)
+        {
+            nPosContext.StockItems.Add(new()
+            {
+                ProductId = stockItem.ProductId,
+                NumberInStock = stockItem.NumberInStock,
+                Cost = stockItem.Cost,
+                CreatedAt = DateTime.UtcNow
+            });
+
+            await nPosContext.SaveChangesAsync();
+        }
+    }
+}
