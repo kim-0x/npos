890700fb2fd78a85cf609ccc2fff37b43032350b
lkimleng <limk@hhbrown.com>
2026-09-11 12:37:18 -0400

    Add defined a product repository interface for all operations

---
+++ src/NPos.Application/Model/ProductQuery.cs
@@ -0,0 +1,4 @@
+﻿namespace NPos.Application.Model
+{
+    public record ProductQuery(Guid Id, string Barcode);
+}
---
+++ src/NPos.Application/Repository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using NPos.Application.Model;
+using NPos.Inventory.Model;
+
+namespace NPos.Application.Repository
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductBy(ProductQuery query);
+        Task<Product[]> GetProducts();
+        Task SaveProduct(Product product);
+    }
+}
