8bb04a4c6ee7fcd3965d612415718078b36ae094
lkimleng <limk@hhbrown.com>
2026-09-11 16:23:50 -0400

    Add implement product repository using EFCore

---
+++ src/NPos.Infrastructure/Repository/ProductRepository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NPos.Infrastructure.Persistence;
+using NPos.Inventory.Abstraction.Repository;
+using NPos.Inventory.Model;
+using NPos.Inventory.Queries;
+
+namespace NPos.Infrastructure.Repository
+{
+    public class ProductRepository(NPosContext nPosContext) : IProductRepository
+    {
+        public async Task<Product?> GetProductBy(ProductQuery query)
+        {
+            return await nPosContext.Products
+                 .AsNoTracking()
+                 .Where(p => (query.Id == null || p.Id == query.Id || p.Barcode == query.Barcode))
+                 .Include(p => p.Category)
+                 .Select(p => new Product
+                 {
+                     Id = p.Id,
+                     Barcode = p.Barcode,
+                     Name = p.Name,
+                     Category = ProductCategory.FromString(p.Category.Name)
+                 })
+                 .FirstOrDefaultAsync();
+        }
+
+        public Task<Product[]> GetProducts()
+        {
+            return nPosContext.Products
+                .AsNoTracking()
+                .Include(p => p.Category)
+                .Take(1000)
+                .Select(p => new Product
+                {
+                    Id = p.Id,
+                    Barcode = p.Barcode,
+                    Name = p.Name,
+                    Category = ProductCategory.FromString(p.Category.Name)
+                })
+                .ToArrayAsync();
+        }
+
+        public async Task SaveProduct(Product product)
+        {
+            if (product.Category is null)
+            {
+                nPosContext.Products.Add(new Entity.Product
+                {
+                    Id = product.Id,
+                    Barcode = product.Barcode,
+                    Name = product.Name,
+                });
+            }
+            else
+            {
+                var category = await nPosContext.Categories
+                    .AsNoTracking()
+                    .Where(c => c.Name.ToLower() == product.Category.ToString())
+                    .FirstOrDefaultAsync();
+
+                nPosContext.Products.Add(new Entity.Product
+                {
+                    Id = product.Id,
+                    Barcode = product.Barcode,
+                    Name = product.Name,
+                    Category = new Entity.Category
+                    {
+                        Id = category?.Id ?? 0,
+                        Name = product.Category.ToString()
+                    }
+                });
+            }
+
+            await nPosContext.SaveChangesAsync();
+        }
+    }
+}
