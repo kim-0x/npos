ded46040ac94e905fa911e1e0fbbc6066c1a9faf
lkimleng <limk@hhbrown.com>
2026-09-11 14:17:39 -0400

    Add defined stock item interface for operations including get items, current stock level, price and save item.

---
+++ src/NPos.Application/Repository/IStockItemRepository.cs
@@ -0,0 +1,12 @@
+﻿using NPos.Inventory.Model;
+
+namespace NPos.Application.Repository
+{
+    public interface IStockItemRepository
+    {
+        Task<StockItem[]> GetStockItemsByProductId(Guid productId);
+        Task<double> GetCurrentStockLevelByProductId(Guid productId);
+        Task<decimal> GetLatestStockPriceByProductId(Guid productId);
+        Task SaveStockItem(StockItem stockItem);
+    }
+}
