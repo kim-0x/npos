a06e405693fbee0d7ab25dd5b48e3ab6c9718f96
lkimleng <limk@hhbrown.com>
2026-09-11 11:34:50 -0400

    Add product category

---
+++ src/NPos.Inventory/Model/ProductCategory.cs
@@ -0,0 +1,44 @@
+﻿namespace NPos.Inventory.Model
+{
+    public sealed class ProductCategory
+    {
+        public static ProductCategory FOOD => new("food");
+        public static ProductCategory BEVERAGE => new("beverage");
+        public static ProductCategory HOUSEHOLD => new("household");
+        public static ProductCategory FRUIT => new("fruit");
+        public static ProductCategory DAIRY => new("dairy");
+
+        public string Value { get; }
+        private ProductCategory(string value)
+        {
+            Value = value;
+        }
+
+        public static IReadOnlyCollection<ProductCategory> All { get; } =
+        [
+            FOOD,
+            BEVERAGE,
+            HOUSEHOLD,
+            FRUIT,
+            DAIRY
+        ];
+
+        public static ProductCategory FromString(string value)
+        {
+            foreach (var category in All)
+            {
+                if (category.Value.Equals(value, StringComparison.OrdinalIgnoreCase))
+                {
+                    return category;
+                }
+            }
+
+            throw new ArgumentException($"Unknown category: '{value}'");
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
