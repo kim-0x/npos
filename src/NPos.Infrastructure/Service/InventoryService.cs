592f4e7de9218845d50bdd02a4e8b852d3b8bf06
lkimleng <limk@hhbrown.com>
2026-09-11 15:20:36 -0400

    Add first iteration 1 for implement inventory service including create new product, get product cost, and stock entry

---
+++ src/NPos.Infrastructure/Service/InventoryService.cs
@@ -0,0 +1,49 @@
+﻿using NPos.Application.Model;
+using NPos.Application.Repository;
+using NPos.Application.Service;
+using NPos.Inventory.Exceptions;
+using NPos.Inventory.Model;
+
+namespace NPos.Infrastructure.Service
+{
+    public class InventoryService(
+        IProductRepository productRepository,
+        IStockItemRepository stockItemRepository) : IInventoryService
+    {
+        public async Task CreateNewProduct(string barcode, string name, ProductCategory category)
+        {
+            Product product = new()
+            {
+                Barcode = barcode,
+                Name = name,
+                Category = category
+            };
+
+            await productRepository.SaveProduct(product);
+        }
+
+        public async Task<decimal> GetProductCostBy(ProductQuery query)
+        {
+            var product = await productRepository.GetProductBy(query)
+                ?? throw new ProductNotFoundException($"Product with barcode '{query.Barcode}' not found in product catalog.");
+
+            return await stockItemRepository.GetLatestItemCostById(product.Id);
+        }
+
+        public async Task StockEntry(string barcode, decimal cost, double numberInStock)
+        {
+            ProductQuery query = new(null, barcode);
+            var product = await productRepository.GetProductBy(query)
+                ?? throw new ProductNotFoundException($"Product with barcode '{query.Barcode}' not found in product catalog.");
+
+            StockItem stockItem = new()
+            {
+                ProductId = product.Id,
+                Cost = cost,
+                NumberInStock = numberInStock
+            };
+
+            await stockItemRepository.SaveStockItem(stockItem);
+        }
+    }
+}
