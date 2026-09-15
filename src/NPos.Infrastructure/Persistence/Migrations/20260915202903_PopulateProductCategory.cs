e0f82568ec33512ec8260a786d27171419d45bd3
lkimleng <limk@hhbrown.com>
2026-09-15 16:33:55 -0400

    Add pre-populate product category into database

---
+++ src/NPos.Infrastructure/Persistence/Migrations/20260915202903_PopulateProductCategory.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NPos.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateProductCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                SET IDENTITY_INSERT Categories ON;
+
+                INSERT INTO Categories (Id, Name) VALUES 
+                (1, 'Food'),
+                (2, 'Beverage'),
+                (3, 'Household'),
+                (4, 'Fruit'),
+                (5, 'Dairy'),
+                (6, 'Frozen Foods'),
+                (7, 'Meat'),
+                (8, 'Produce'),
+                (9, 'Cleaners'),
+                (10, 'Personal Care');
+
+                SET IDENTITY_INSERT Categories OFF;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categories WHERE Id IN (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);");
+        }
+    }
+}
