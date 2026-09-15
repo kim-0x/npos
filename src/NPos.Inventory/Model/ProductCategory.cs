712e7a71b84c7858e21705d6fed56a762491c2ca
lkimleng <limk@hhbrown.com>
2026-09-15 17:33:44 -0400

    Add enforce naming rule to product category as capitialize

--- src/NPos.Inventory/Model/ProductCategory.cs
+++ src/NPos.Inventory/Model/ProductCategory.cs
@@ -1,8 +1,26 @@
-﻿namespace NPos.Inventory.Model
+﻿using System.Globalization;
+
+namespace NPos.Inventory.Model
 {
     public class ProductCategory
     {
+        private string _name = string.Empty;
+
         public int Id { get; set; }
-        public required string Name { get; set; }
+        public required string Name
+        {
+            get => _name;
+            set => _name = Capitalize(value);
+        }
+
+        private static string Capitalize(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return value;
+            }
+
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(value.ToLower());
+        }
     }
 }
